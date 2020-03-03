3159bd888b7859093acf06b1b97abca556257ec3
Christoph Frick <cf@ofnir.net>
2020-03-03 12:49:53 +0100

    Update versions and minor adjustments
    - use Groovy 3
    - use the vaadin spring starter (to get the version from BOM)
    - update gradle vaadin plugin

--- src/main/groovy/net/ofnir/v14gradle/V14gradleApplication.groovy
+++ src/main/groovy/net/ofnir/v14gradle/V14gradleApplication.groovy
@@ -1,10 +1,10 @@
 package net.ofnir.v14gradle
 
 import com.vaadin.flow.component.Composite
-import com.vaadin.flow.component.combobox.ComboBox
 import com.vaadin.flow.component.html.Div
 import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.router.Route
+import com.vaadin.flow.spring.annotation.EnableVaadin
 import com.vaadin.flow.theme.Theme
 import com.vaadin.flow.theme.lumo.Lumo
 import org.springframework.boot.SpringApplication
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.stereotype.Service
 
 @SpringBootApplication
+@EnableVaadin
 class V14gradleApplication {
 
     static void main(String[] args) {
@@ -21,17 +22,14 @@ class V14gradleApplication {
 }
 
 @Service
-class LolService {
-    String lol() { "lol" }
+class HelloService {
+    String sayHello() { "Hello" }
 }
 
 @Route("")
 @Theme(value = Lumo)
 class MainView extends Composite<Div> {
-    MainView(LolService lolService) {
-        content.add(new H1(lolService.lol()))
-        content.add(new ComboBox<String>().tap{
-            setItems(["1", "2", "3", "4", "5"])
-        })
+    MainView(HelloService lolService) {
+        content.add(new H1(lolService.sayHello()))
     }
 }
