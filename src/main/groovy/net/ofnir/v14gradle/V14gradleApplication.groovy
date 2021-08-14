ae7037dd8c17a7939e3ccf838ca116fa2d3179e0
Christoph Frick <cf@ofnir.net>
2021-08-14 14:06:40 +0200

    Initial draft of the binder field

--- src/main/groovy/net/ofnir/v14gradle/V14gradleApplication.groovy
+++ src/main/groovy/net/ofnir/v14gradle/V14gradleApplication.groovy
@@ -1,21 +1,34 @@
 package net.ofnir.v14gradle
 
+
+import com.vaadin.flow.component.AbstractField
+import com.vaadin.flow.component.Component
 import com.vaadin.flow.component.Composite
-import com.vaadin.flow.component.Text
+import com.vaadin.flow.component.HasValueAndElement
 import com.vaadin.flow.component.button.Button
-import com.vaadin.flow.component.button.ButtonVariant
+import com.vaadin.flow.component.datepicker.DatePicker
 import com.vaadin.flow.component.formlayout.FormLayout
 import com.vaadin.flow.component.html.Div
-import com.vaadin.flow.component.html.H1
+import com.vaadin.flow.component.notification.Notification
 import com.vaadin.flow.component.page.BodySize
 import com.vaadin.flow.component.page.Viewport
+import com.vaadin.flow.component.textfield.EmailField
 import com.vaadin.flow.component.textfield.TextField
+import com.vaadin.flow.data.binder.BeanValidationBinder
+import com.vaadin.flow.data.binder.Binder
 import com.vaadin.flow.router.Route
+import com.vaadin.flow.shared.Registration
 import com.vaadin.flow.spring.annotation.EnableVaadin
 import com.vaadin.flow.theme.Theme
+import groovy.transform.Canonical
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.stereotype.Service
+
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Past
+import java.time.LocalDate
 
 @SpringBootApplication
 @EnableVaadin
@@ -27,33 +40,172 @@ class V14gradleApplication {
 
 }
 
-@Service
-class HelloService {
-    String sayHello(String to) { "Hello, ${to ?: "World"}" }
-}
-
 @Route("")
 @Theme(themeFolder = 'app-theme')
 @Viewport(Viewport.DEVICE_DIMENSIONS)
 @BodySize(height = "100vh", width = "100vw")
 class MainView extends Composite<Div> {
-    MainView(HelloService lolService) {
-        TextField tf = new TextField().tap {
-            placeholder = "Please enter a name to greet"
-            id = "name-input"
-        }
-        Button b = new Button("Greet!").tap {
-            addThemeVariants(ButtonVariant.LUMO_PRIMARY)
-            id = "greet-button"
-            addClickListener {
-                content.add(new Div(new Text(lolService.sayHello(tf.value))))
-            }
-        }
+    MainView() {
+        def pairField = new PairField()
+        pairField.value = new Pair()
         content.tap {
-            add(new H1("Greeting Service"))
-            add(new FormLayout(tf, b).tap{
-                responsiveSteps = [new FormLayout.ResponsiveStep("0px", 1)]
+            add(pairField)
+            add(new Button("Show").tap {
+                addClickListener {
+                    pairField.binder.validate()
+                    Notification.show(pairField.value.toString())
+                }
             })
         }
+    }
+}
+
+abstract class BindingForm<C extends Component, D> extends Composite<C> implements HasValueAndElement<AbstractField.ComponentValueChangeEvent<C, D>, D> {
+
+    final Class<D> clazz
+    final Binder<D> binder
+    boolean readOnly = false
+
+    abstract protected void buildForm()
+
+    abstract protected void bindFields(Binder<D> binder)
+
+    BindingForm(Class<D> clazz) {
+        this.clazz = clazz
+        this.binder = buildBinder()
+        buildForm()
+        bindFields(binder)
+    }
+
+    protected Binder<D> buildBinder() {
+        new Binder<D>(clazz)
+    }
+
+    @Override
+    void setValue(D value) {
+        binder.setBean(value)
+    }
+
+    @Override
+    D getValue() {
+        binder.getBean()
+    }
+
+    @Override
+    Registration addValueChangeListener(ValueChangeListener listener) {
+        binder.addValueChangeListener(listener)
+    }
+
+    @Override
+    D getEmptyValue() {
+        clazz.getDeclaredConstructor().newInstance()
+    }
+
+    @Override
+    void clear() {
+        // TODO: binder.clearFields()
+    }
+
+    @Override
+    void setReadOnly(boolean readOnly) {
+        this.readOnly = readOnly
+        binder.setReadOnly(readOnly)
+    }
+
+    @Override
+    void setRequiredIndicatorVisible(boolean requiredIndicatorVisible) {
+        // TODO
+    }
+
+    @Override
+    boolean isRequiredIndicatorVisible() {
+        // TODO
+        false
+    }
+
+}
+
+@Canonical
+class Person {
+    @NotEmpty
+    String firstName
+    @NotEmpty
+    String lastName
+    @Email
+    String email
+    @Past
+    @NotNull
+    LocalDate dayOfBirth
+}
+
+class PersonField extends BindingForm<FormLayout, Person> {
+
+    PersonField() {
+        super(Person)
+    }
+
+    private TextField firstName
+    private TextField lastName
+    private EmailField email
+    private DatePicker dayOfBirth
+
+    @Override
+    protected void buildForm() {
+        content.tap {
+            add(firstName = new TextField("First name"))
+            add(lastName = new TextField("Last name"))
+            add(email = new EmailField("Email"))
+            add(dayOfBirth = new DatePicker("Date of birth"))
+        }
+    }
+
+    @Override
+    protected void bindFields(Binder<Person> binder) {
+        binder.forField(firstName).bind("firstName")
+        binder.forField(lastName).bind("lastName")
+        binder.forField(email).bind("email")
+        binder.forField(dayOfBirth).bind("dayOfBirth")
+    }
+
+    @Override
+    protected Binder<Person> buildBinder() {
+        new BeanValidationBinder<Person>(Person)
+    }
+}
+
+@Canonical
+class Pair {
+    @NotNull
+    Person a = new Person()
+
+    @NotNull
+    Person b = new Person()
+}
+
+class PairField extends BindingForm<FormLayout, Pair> {
+
+    PairField() {
+        super(Pair)
+    }
+
+    private PersonField a, b
+
+    @Override
+    protected void buildForm() {
+        content.tap {
+            add(a = new PersonField())
+            add(b = new PersonField())
+        }
+    }
+
+    @Override
+    protected void bindFields(Binder<Pair> binder) {
+        binder.forField(a).bind("a")
+        binder.forField(b).bind("b")
+    }
+
+    @Override
+    protected Binder<Pair> buildBinder() {
+        new BeanValidationBinder<Pair>(Pair)
     }
 }
